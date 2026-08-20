ddf12e3151fea0a76b2badd96743a82ba600970a
SimpleFeral <rolandmayer01@web.de>
2026-08-20 22:20:42 +0200

    Excersise 4a

---
+++ Aufgabe_4.py
@@ -0,0 +1,38 @@
+from xml.dom.minidom import parse, Node
+
+class Book:
+    title: str
+    author: str
+    
+    def __init__(self, title: str, author: str):
+        self.title = title
+        self.author = author
+        
+    def __eq__(self, other):
+        return (type(other) == Book 
+                and self.title == other.title 
+                and self.author == self.author)
+        
+    def __str__(self):
+        return f'Book: (Title: {self.title}, Author: {self.author})'
+        
+books_xml = parse('books.xml')
+
+def resolve_title_and_author():
+    resolved_books = [Book]
+    for book_node in books_xml.getElementsByTagName('book'):
+        title = ''
+        author = ''
+        for attributeNode in book_node.childNodes:
+            if attributeNode.nodeType == Node.ELEMENT_NODE:
+                if attributeNode.nodeName == "title":
+                    title = attributeNode.childNodes[0].data.strip()
+                if attributeNode.nodeName == "author":
+                    author = attributeNode.childNodes[0].data.strip()
+        book = Book(title, author)
+        if book not in resolved_books:
+            resolved_books.append(book)
+    return resolved_books
+            
+for book in resolve_title_and_author():
+    print(book)
